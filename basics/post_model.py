baf537493d804c9a75b3e6e53833b6f3db54f9e4
jahidcs <csq.jahid@gmail.com>
2024-05-03 23:58:22 +0600

    repository refactoring

--- basics/post_model.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = False
-    rating: Optional[float] = None
